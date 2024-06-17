85a3a9192fc3aee65f97e7e2bda957489c75204a
Mayokun-Sofowora <sofoworamayokun@gmail.com>
2024-06-17 19:35:09 +0200

    Update files

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,20 +1,20 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer className='border-t'>
-      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
-        <Link href={'/'}>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
           <Image 
-          src={'/assets/images/linkup_logo.svg'} 
-          alt='logo'
-          width={58}
-          height={38}
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
           />
         </Link>
-        <p>&copy; 2024 LinkUp. All Rights Reserved.</p>
+
+        <p>2023 Evently. All Rights reserved.</p>
       </div>
     </footer>
   )
